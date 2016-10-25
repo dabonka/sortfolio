694963b8b7253a79db56f0d56a7ed26254c88860
Roman <dabonka@yandex.ru>
2016-10-25 23:23:58 +0300

    add relations

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,2 +1,4 @@
 class Picture < ApplicationRecord
+  belongs_to  :user
+  has_one     :portfolio
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_one :portfolio, dependent: :destroy
+  has_many :pictures, through: portfolio,  dependent: :destroy
+  validates :email, uniqueness: true
 end
